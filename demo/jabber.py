4622ee047e5ea5be6758e4cbaeb5177b1e857cc9
vvk6rd <vvk6rd@virginia.edu>
2023-10-24 22:57:45 -0400

    completed

---
+++ demo/jabber.py
@@ -0,0 +1,6 @@
+def what_class_is_this()
+
+	"""For module 9 classwork."""
+	print("This work is for module 9 testing")def what_class_is_this()
+
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name = 'Demo',
+	version = '0.1',
+	description = 'A simple demonstration package',
+	url='https://github.com/vvk6rd/classwork_mod9',
+	author = 'Ade Faparusi',
+	author_email = 'vvk6rd@virginia.edu',
+	license= 'MIT',
+	packages= ['demo']
+)
